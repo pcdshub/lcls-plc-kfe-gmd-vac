78e225869ad763fcaf0341b5b83292f9d66731f7
ghalym <mghaly@slac.stanford.edu>
2021-01-11 16:44:40 -0800

    BLD: new IOC with the latest IOC version, ENH: added the new roughing valve to the ui.

--- plc/iocBoot/ioc-plc-kfe-gmd-vac/load_plc_databases.cmd
+++ plc/iocBoot/ioc-plc-kfe-gmd-vac/load_plc_databases.cmd
@@ -1,1 +1,1 @@
-dbLoadRecords("plc_kfe_gmd_vac.db", "PORT=ASYN_PLC,PREFIX=IOC:PLC:KFE:GMD:VAC:,IOCNAME=$(IOCNAME)")
+dbLoadRecords("plc_kfe_gmd_vac.db", "PORT=ASYN_PLC,PREFIX=IOC:PLC:KFE:GMD:VAC:,IOCNAME=$(IOC),IOC=$(IOC)")
